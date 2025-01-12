65eff29097aebf999b8d9166493ad4e19434975d
Adam <adamziv156@gmail.com>
2025-01-12 09:21:44 -0600

    added unit tests for Http versions

--- src/test/java/org/example/http/HttpVersionTest.java
+++ src/test/java/org/example/http/HttpVersionTest.java
@@ -1,5 +1,47 @@
 package org.example.http;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class HttpVersionTest {
 
+
+    @Test
+    void getBestCompatibleVersionExactMatch() {
+        HttpVersion httpVersion = null;
+        try {
+            httpVersion = HttpVersion.getBestCompatibleVersion("HTTP/1.1");
+        } catch (BadHttpVersionException e) {
+
+            fail();
+        }
+
+        assertNotNull(httpVersion);
+        assertEquals(HttpVersion.HTTP_1_1, httpVersion);
+    }
+
+    @Test
+    void getBestCompatibleVersionBadFormat() {
+        HttpVersion httpVersion = null;
+        try {
+            httpVersion = HttpVersion.getBestCompatibleVersion("http/1.1");
+            fail();
+        } catch (BadHttpVersionException e) {
+
+        }
+    }
+
+    @Test
+    void getBestCompatibleVersionHigherVersion() {
+        HttpVersion httpVersion = null;
+        try {
+            httpVersion = HttpVersion.getBestCompatibleVersion("HTTP/1.2");
+        } catch (BadHttpVersionException e) {
+            fail();
+
+        }
+        assertNotNull(httpVersion);
+        assertEquals(HttpVersion.HTTP_1_1, httpVersion);
+    }
 }
